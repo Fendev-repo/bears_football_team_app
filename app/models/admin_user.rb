b7a84f46fd7743508d04e7d537a4e6b1b8966ced
Fen <fendev@protonmail.com>
2019-02-27 15:15:38 +1100

    integrated active admin

---
+++ app/models/admin_user.rb
@@ -0,0 +1,7 @@
+class AdminUser < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable,
+         :recoverable, :rememberable, :validatable
+end
+user = AdminUser.create :email => 'user2@example.com', :password => 'please'
--- spec/features/sign_out_spec.rb
+++ spec/features/sign_out_spec.rb
@@ -8,7 +8,7 @@ RSpec.feature "SignOuts", type: :feature do
     sign_in user
     visit root_path
     click_link 'Sign out'
-    
+
     expect(page).to have_content('Signed out successfully.')
     expect(page).to have_content('Sign in')
   end

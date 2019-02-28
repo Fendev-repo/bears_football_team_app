f41a51a876b0ed7e0230e39a6bff3f6da7ded207
Fen <fendev@protonmail.com>
2019-02-28 14:00:18 +1100

    added seed data for database for users

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+# AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+
+case Rails.env
+when "development"
+  # ActiveAdmin default admin user
+   AdminUser.create!(email: 'admin@example.com',
+     password: 'password',
+     password_confirmation: 'password')
+
+  # 10 standard 'devise confirmed' users 
+   10.times do |num|
+     User.create(first_name: "#{['jenni', 'sandi', 'john', 'bill'].sample}",
+                  last_name: "#{['williams', 'white', 'jones'].sample}",
+                  email: "test#{num}@test.com",
+                  password: "#{num}password")
+   end
+   User.all.each do |user|
+     user.confirm
+   end
+when "production"
+end
